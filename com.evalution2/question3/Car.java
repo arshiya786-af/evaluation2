5987179a9b96dc2abd5611837267b94a06d44544
Arshiya Moin fw16-409 <73097366+arshiya786-af@users.noreply.github.com>
2022-05-30 16:20:01 +0530

    Update Car.java

--- com.evalution2/question3/Car.java
+++ com.evalution2/question3/Car.java
@@ -1,7 +1,23 @@
 package question3;
 
-public class Car {
-	int numberOfPassenger ;
-	int numberOfKms;
-	String name;
+import java.util.Scanner;
+class Car{
+	
+	private int numberOfPassenger;
+	private int numberOfKms;
+	
+	public int getNumberOfPassenger() {
+		return numberOfPassenger;
+	}
+	public void setNumberOfPassenger(int numberOfPassenger) {
+		this.numberOfPassenger = numberOfPassenger;
+	}
+	public int getNumberOfKms() {
+		return numberOfKms;
+	}
+	public void setNumberOfKms(int numberOfKms) {
+		this.numberOfKms = numberOfKms;
+	}
+	
+	
 }
