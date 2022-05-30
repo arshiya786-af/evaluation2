3282c23eac14204b0c93367b6516c3a9461a91d1
Arshiya Moin fw16-409 <73097366+arshiya786-af@users.noreply.github.com>
2022-05-30 16:23:06 +0530

    Create Main.java

---
+++ com.evalution2/question3/Main.java
@@ -0,0 +1,26 @@
+package question3
+  public class Main {
+
+	public static void main(String[] args) {
+
+		
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter number of passengers:");
+		int n=s.nextInt();
+		
+		System.out.println("Enter number of kms:");
+		int km=s.nextInt();
+		
+		Car car= new Car();
+		car.setNumberOfKms(km);
+		car.setNumberOfPassenger(n);
+		
+		OLA ola= new OLA();
+		//ola.bookCar(n, km);
+		Car c=ola.bookCar(n, km);
+		ola.calculateBill(c);
+		//int ans= ola.calculateBill(c);
+		//System.out.println("The total fare amount is "+ ans);
+	}
+
+}
